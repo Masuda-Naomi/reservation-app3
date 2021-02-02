c7adc3c56f0bf8708536f86b5b33fedb40982641
Masuda <n.masuda@fujinetworks.co.jp>
2021-02-02 11:35:55 +0900

    Section9

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const config = require('./config/dev')
+const config = require('./config/index')
 const SampleDb = require('./sample-db')
 
 const productRoutes = require('./routes/products')
+const path = require('path')
 
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
+  // useCreateIndex: true
 }).then(
   () => {
-    const sampleDb = new SampleDb()
-    sampleDb.initDb()
+    if(process.env.NODE_ENV !== 'production') {
+      const sampleDb = new SampleDb()
+      // sampleDb.initDb()
+    }
   }
 )
 
@@ -24,6 +27,13 @@ app.use('/api/v1/products', productRoutes)
 //   res.json({'success': true})
 // })
 
+if(process.env.NODE_ENV === 'production') {
+  const appPath = path.join( __dirname,'..', 'dist', 'reservation-app3')
+  app.use(express.static(appPath))
+  app.get("*", function(req, res) {
+    res.sendFile(path.resolve(appPath, 'index.html'))
+  })
+}
 const PORT = process.env.PORT || '3001'
 
 app.listen(PORT, function() {
