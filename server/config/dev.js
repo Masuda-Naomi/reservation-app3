e1d5b23f9b4fbb4fdbb0c13d87f065120a90a670
Masuda <n.masuda@fujinetworks.co.jp>
2021-02-02 11:51:33 +0900

    Section10

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  DB_URI: 'mongodb+srv://test:testtest@cluster0.zkzev.mongodb.net/<dbname>?retryWrites=true&w=majority'
+  DB_URI: 'mongodb+srv://test:testtest@cluster0.zkzev.mongodb.net/test?retryWrites=true&w=majority'
   // DB_URI: 'mongodb+srv://dbReserve:testtest@cluster0.mh4zo.mongodb.net/test?retryWrites=true&w=majority'
 }
 //3行目、password→testtest1からtesttest、<dbname>→testに変更（本番用）
